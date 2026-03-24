fefef7456c078caa002aaef80a669e04e8a695c2
LordKakuno <30099789@tafe.wa.edu.au>
2026-03-24 14:59:48 +0800

    Refined Search algorithm
    I have changed the the output

--- MarlinSatelliteDataProcessor/MainWindow.xaml.cs
+++ MarlinSatelliteDataProcessor/MainWindow.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
 using System.Windows.Media.Animation;
 
 namespace MarlinSatelliteDataProcessor
@@ -24,21 +28,45 @@ namespace MarlinSatelliteDataProcessor
         {
             ShowAllData();
             NumberOfNodes();
-            
+
         }
 
         private void btnSearchRecursiveA_Click(object sender, RoutedEventArgs e)
         {
-            int left = Math.Min(0, sensorAList.Count - 1);
-            int right = Math.Max(0, sensorAList.Count - 1);
-            BinarySearchRecursive(sensorAList, int.Parse(txtSensorAInput.Text), left, right);
+            double target = double.Parse(txtSensorAInput.Text);
+            int left = 0, right = sensorAList.Count - 1;
+            int index = BinarySearchRecursive(sensorAList, target, left, right);
 
+            lstSensorA.UnselectAll();
+
+            if (index != -1)
+            {
+                lstSensorA.SelectedItems.Add(lstSensorA.Items[index]);
+            }
+            else
+            {
+                MessageBox.Show("Target not found.");
+            }
         }
 
         private void btnSearchInterativeA_Click(object sender, RoutedEventArgs e)
         {
-            BinarySearchIterative(sensorAList, int.Parse(txtSensorAInput.Text));
+            double target = double.Parse(txtSensorAInput.Text);
+
+            int index = BinarySearchIterative(sensorAList, target);
+
+            lstSensorA.UnselectAll();
+
+            if (index != -1)
+            {
+                lstSensorA.SelectedItems.Add(lstSensorA.Items[index]);
+            }
+            else
+            {
+                MessageBox.Show("Target not found.");
+            }
         }
+
 
         private void btnSortA_Click(object sender, RoutedEventArgs e)
         {
@@ -52,16 +80,37 @@ namespace MarlinSatelliteDataProcessor
 
         private void btnSearchRecursiveB_Click(object sender, RoutedEventArgs e)
         {
-            var left = Math.Min(0, sensorBList.Count - 1);
-            var right = Math.Max(0, sensorBList.Count - 1);
-            BinarySearchRecursive(sensorBList, int.Parse(txtSensorBInput.Text), left, right);
+            string input = txtSensorBInput.Text;
+            int index = -1;
+            lstSensorB.SelectedItems.Clear();
+            if (int.TryParse(input, out int intTarget))
+            {
+                int left = 0, right = sensorBList.Count - 1;
+                Stopwatch sw = Stopwatch.StartNew();
+                index = BinarySearchRecursive(sensorBList, intTarget, left, right);
+                sw.Stop();
+                HighlightRange(sensorBList, intTarget, lstSensorB);
+            }
+            else if (double.TryParse(input, out double doubleTarget))
+            {
+                int left = 0, right = sensorBList.Count - 1;
+                Stopwatch sw = Stopwatch.StartNew();
+                index = BinarySearchRecursive(sensorBList, doubleTarget, left, right);
+                lstSensorB.SelectedItems.Add(lstSensorB.Items[index]);
+                sw.Stop();
+            }
+            else
+            {
+                MessageBox.Show("Invalid input. Please enter a number.");
+                return;
+            }
         }
 
         private void btnSearchInterativeB_Click(object sender, RoutedEventArgs e)
         {
-            var left = Math.Min(0, sensorBList.Count - 1);
-            var right = Math.Max(0, sensorBList.Count - 1);
-            BinarySearchRecursive(sensorBList, int.Parse(txtSensorBInput.Text), left, right);
+            double target = double.Parse(txtSensorBInput.Text), min = target, max = target + 1;
+
+
         }
 
         private void btnSortB_Click(object sender, RoutedEventArgs e)
@@ -105,7 +154,7 @@ namespace MarlinSatelliteDataProcessor
 
             sensorAList.Clear();
             sensorBList.Clear();
-            
+
             for (int i = 0; i < SIZE; i++)
             {
                 sensorAList.AddLast(readData.SensorA(mu, sigma));
@@ -117,7 +166,6 @@ namespace MarlinSatelliteDataProcessor
         }
         private void ShowAllData()
         {
-            LoadData();
             if (!LoadData())
                 return;
 
@@ -136,28 +184,28 @@ namespace MarlinSatelliteDataProcessor
                 lstViewSatelliteData.Items.Add(new SatelliteRow { SensorA = a[i], SensorB = b[i] });
             }
         }
-
-        private void NumberOfNodes()
+        private (int countA, int countB) NumberOfNodes()
         {
             int countA = sensorAList.Count;
             int countB = sensorBList.Count;
             mainToolBar.Items.Clear();
-            mainToolBar.Items.Add(new System.Windows.Controls.Label { Content = $"Sensor A Nodes: {countA}" });
-            mainToolBar.Items.Add(new System.Windows.Controls.Label { Content = $"Sensor B Nodes: {countB}" });
+            mainToolBar.Items.Add(new Label { Content = $"Sensor A Nodes: {countA}" });
+            mainToolBar.Items.Add(new Label { Content = $"Sensor B Nodes: {countB}" });
+            return (countA, countB);
         }
         private void DisplayListBoxData()
         {
             lstSensorA.Items.Clear();
             lstSensorB.Items.Clear();
-            foreach (var data in sensorAList) lstSensorA.Items.Add(data.ToString("F3"));
-            foreach (var data in sensorBList) lstSensorB.Items.Add(data.ToString("F3"));
+            foreach (var data in sensorAList) lstSensorA.Items.Add(data.ToString("F4"));
+            foreach (var data in sensorBList) lstSensorB.Items.Add(data.ToString("F4"));
         }
         private bool SelectionSort(LinkedList<double> list)
         {
             if (list.Count == 0) return false;
             int min = 0;
             int max = list.Count - 1;
-            for (int i = 0; i < max; i++)
+            for (int i = 0; i <= max; i++)
             {
                 min = i;
                 for (int j = i + 1; j < list.Count; j++)
@@ -177,75 +225,81 @@ namespace MarlinSatelliteDataProcessor
         private bool InsertionSort(LinkedList<double> list)
         {
             if (list.Count == 0) return false;
-            int min = 0;
-            int max = list.Count - 1;
-            for (int i = 1; i <= max; i++)
+            int max = list.Count;
+            for (int i = 0; i <= max - 1; i++)
             {
-                min = i;
-                for (int j = i + 1; j > 0; j--)
+                for (int j = i; j > 0; j--)
                 {
                     if (list.ElementAt(j - 1) > list.ElementAt(j))
                     {
-                        LinkedListNode<double> currentNode = list.Find(list.ElementAt(j));
-                        LinkedListNode<double> previousNode = list.Find(list.ElementAt(j - 1));
-                        var temp = currentNode.Value;
-                        currentNode.Value = previousNode.Value;
-                        previousNode.Value = temp;
-
+                        LinkedListNode<double> nodeA = list.Find(list.ElementAt(j - 1));
+                        LinkedListNode<double> nodeB = list.Find(list.ElementAt(j));
+                        var temp = nodeA.Value;
+                        nodeA.Value = nodeB.Value;
+                        nodeB.Value = temp;
                     }
-                    else
-                        break;
+                    else break;
                 }
             }
             DisplayListBoxData();
             return isSorted = true;
         }
-        private int BinarySearchRecursive(LinkedList<double> list, int target, int left, int right)
+        private int BinarySearchRecursive(LinkedList<double> list, double target, int left, int right)
         {
-            if (isSorted)
-            {
-                if (left > right)
-                    return -1;
-                int mid = left + (right - left) / 2;
-                var midValue = list.ElementAt(mid);
-                if (Math.Abs(midValue - target) < 0.1)
-                    return mid;
-                else if (midValue < target)
-                    return BinarySearchRecursive(list, target, mid + 1, right);
-                else
-                    return BinarySearchRecursive(list, target, left, mid - 1);
-            }
-            else
+            if (!isSorted)
             {
                 MessageBox.Show("Please sort the list before performing a binary search.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return -1;
             }
+            else if (left <= right)
+            {
+                int mid = (left + right);
+                if ((int)list.ElementAt(mid) == target)
+                    return mid;
+                else if (target < list.ElementAt(mid))
+                    return BinarySearchRecursive(list, target, left, mid - 1);
+                else
+                    return BinarySearchRecursive(list, target, mid + 1, right);
+            }
+            return left;
 
         }
         private int BinarySearchIterative(LinkedList<double> list, double target)
         {
-            if (isSorted)
+            int min = 0;
+            int max = list.Count;
+            if (!isSorted)
             {
-                int left = 0, right = list.Count - 1;
-                while (left <= right)
-                {
-                    int mid = left + (right - left) / 2;
-                    var midValue = list.ElementAt(mid);
-                    if (midValue == target)
-                        return mid;
-                    else if (midValue < target)
-                        left = mid + 1;
-                    else
-                        right = mid - 1;
-                }
+                MessageBox.Show("Please sort before searching", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 return -1;
             }
-            else
+            while (min <= max - 1)
             {
-                MessageBox.Show("Please sort the list before performing a binary search.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return -1;
+                int mid = (min + max) / 2;
+                if ((int)list.ElementAt(mid) == target)
+                    return mid;
+                else if (target < list.ElementAt(mid))
+                    max = mid - 1;
+                else
+                    min = mid + 1;
+            }
+            return min;
+        }
+        private void HighlightRange(LinkedList<double> list, int target, ListBox box)
+        {
+            box.Items.Clear();
+            foreach (var item in list)
+            {
+                ListBoxItem listItem = new ListBoxItem();
+                listItem.Content = item;
+                if ((int)item == target)
+                {
+                    listItem.Background = Brushes.Yellow;
 
+                }
+                box.Items.Add(listItem).ToString("F4");
             }
         }
     }
 }
+
